2867f290e58f72fbc7f45b476bce7aad0439e5be
scmc421 <scmc.421@gmail.com>
2019-02-01 07:46:45 -0500

    practica02 basico angular

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,17 @@
+export class ServerComponent {
+  @Component({
+  })
+  export class ServerComponent {
+  }
+  import { Component } from '@angular/core';
+  @Component({
+  })
+  export class ServerComponent {
+  }
+  import { Component } from '@angular/core';
+  @Component({
+  selector: 'app-server'
+  })
+  export class ServerComponent {
+  }
+}
